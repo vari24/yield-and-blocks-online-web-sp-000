51cebee80e6766f39347d793c7966b65e7cef8f4
Sara Varisara <varisara.n@gmail.com>
2019-02-05 13:40:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -15,4 +15,17 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
   end
 end
 
-def hello
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
