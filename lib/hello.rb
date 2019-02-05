0cb5248db8bd6bdfa5ecd777de3ec4164abaaadb
Sara Varisara <varisara.n@gmail.com>
2019-02-05 13:39:43 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   i = 0
 
   while i < array.length
-    yield(array[i])
+    yield array[i]
     i = i + 1
   end
 end
@@ -14,3 +14,5 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
     puts "Hi, #{name}"
   end
 end
+
+def hello
