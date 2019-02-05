bf2cc7f8c489796d48b139a430a0f0da6530fa21
Sara Varisara <varisara.n@gmail.com>
2019-02-05 13:39:24 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,11 +5,8 @@ def hello_t(array)
     yield(array[i])
     i = i + 1
   end
-    array
-  else
-  puts "Hey! No block was given!"
-  end
 end
+
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
